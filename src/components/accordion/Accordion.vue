10cdc49ad123adae59dcc251faf3856b53e911e3
风熠 <feng5495@gmail.com>
2017-08-11 18:11:40 +0800

    add accordion

---
+++ src/components/accordion/Accordion.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="v-accordion" :class="clsWrapper">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Accordion',
+    props: {
+        size: {
+            type: String,
+            default: 'md'
+        },
+    },
+    computed: {
+        clsWrapper() {
+            const list = [];
+            const { size } = this.$props;
+            list.push('v-wingblank' + `-${size}`);
+            // 
+            return list;
+        }
+    }
+};
+</script>
